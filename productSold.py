515522ca8ffceea844fd6ca782e9c255eca1707a
zeek <ezhernandez25@gmail.com>
2024-08-26 00:44:24 -0700

    Cleaned up script and added a few comments. In the future I might update script to get weekly sales between 2 dates. Makes it easier to review trends while minimizing manual work.

--- productSold.py
+++ productSold.py
@@ -55,8 +55,9 @@ loginButton.click()
 #YYYY-MM-DD
 startDate="2024-02-05"
 endDate="2024-02-18"
-print("Here 3")
+
 driver.get(f'https://manager.lsk.lightspeed.app/stats/productBreakdown/custom/{startDate}/{endDate}')
+print("Product sales")
 
 #Full Table Path
 #tableXPath = '/html/body/div[2]/div/div[2]/section/div[5]/div/table'
@@ -67,20 +68,16 @@ tableXPath = '/html/body/div[2]/div/div[2]/section/div[5]/div/table/tbody'
 tableXPath2 = '//table/tbody'
 tableClass = "table table-condensed table-stripped table-bordered breakdown"
 tableTag = "table"
-print("Here 4")
+
 time.sleep(5)
-#wait = input("Wait for input")
+
 table = driver.find_element(By.XPATH, tableXPath2)
-#table = driver.find_element(By.CLASS_NAME, tableClass)
-#table = driver.find_element(By.TAG_NAME, tableTag)
+
 rows = table.find_elements(By.XPATH, ".//tr")
-print("Here 5")
-print(len(rows))
+print("Number of Rows: " + str(len(rows)))
 
 csvFile = pandas.DataFrame(data=None,columns=('textField', 'posID', 'quantity', 'regular', 'large', 'noSize', 'optionsList'))
-#print(csvFile)
 
-#for i in range(10):
 for i in range(len(rows)):
     posID = 'NA'
     quantity = 'NA'
@@ -89,19 +86,22 @@ for i in range(len(rows)):
     noSize = 'NA'
     optionsList = 'NA'
     firstColumn = rows[i].find_element(By.XPATH, ".//td")
+    #Likely Removed because it errored when searching for quantity in this step. Might look for a way to do it here in the future
     #quantity = rows[i].find_element(By.XPATH, ".//td[3]").text
     
+    #Used for debuggin to see if there is actually any data.
     print(f"Element {i}:{len(rows[i].text)} characters")
     print(f"Quantity: {quantity}")
     
-    if len(firstColumn.text) == 0: #rows[i].is_displayed(): I'd like to use this function but it seems to think some visible rows aren't visible.
+    if len(firstColumn.text) == 0: #rows[i].is_displayed(): I'd like to use this function but it seems to think some visible rows aren't visible. 
         print(f"Sub Element Not visible: {i}", color = 'red')
-        print("Clicking preveious carat to expand table")
+        print("Clicking previous carat to expand table")
         rows[i-1].find_element(By.XPATH, ".//td[1]/a/i").text
         rows[i-1].find_element(By.XPATH, ".//td[1]/a/i").click()
     firstColumnText = firstColumn.text
-    print(firstColumnText);
+    print("First Column Text: " + firstColumnText);
     
+    #Retrieves quantity sold of this specific item
     quantity = rows[i].find_element(By.XPATH, ".//td[3]").text
     
     try:
@@ -112,8 +112,9 @@ for i in range(len(rows)):
         productOptions = firstColumn.find_element(By.XPATH, './/div')
         optionsList = productOptions.text
         #driver.execute_script("arguments[0].setAttribute('style.display', 'none')",firstColumn.find_element(By.XPATH, './/a[@class="ik-popover"]'))
-        #This works but it removes the entire text field not just the pop up. Might have to find soluction that just sets the popup to none.
-        #This was required because the popup would remain up and be in the way and make the carat button unclickable
+        #This works but it removes the entire text field not just the pop up. Might have to find soluction that just sets the popup to none and just closes it instead of deletes the data 
+        #S o you can really review the work at the end of the script. You have to log in and check the same dates.
+        #This was required because the popup would remain up and be in the way and make the carat button unclickable.
         popoverWithOptions = firstColumn.find_element(By.XPATH, './/a[@class="ik-popover"]')
         #driver.execute_script("arguments[0].style.display = 'none';",popoverWithOptions)
         driver.execute_script("arguments[0].style.visibility = 'hidden';",firstColumn.find_element(By.XPATH, './/div'))
@@ -121,6 +122,7 @@ for i in range(len(rows)):
         print(optionsList)
         
         optionsListRows = optionsList.split("\n")
+        #ProductID is enclosed in parenthesis
         searchID = re.search('\((\d+?)\)$', optionsListRows[0])
         if searchID:
             productID = searchID.group(1)        
@@ -139,8 +141,9 @@ for i in range(len(rows)):
             noSize = quantity
         csvFile.loc[len(csvFile)] = [firstColumnText,productID, quantity, regular, large, noSize, optionsList]
     except:
+        #Popover does not exist so we will move on to the next one
         print("popover not found", color = 'red')
-        
+#Prints out a small sample of the resulting file        
 print(csvFile, color = 'yellow')
-csvFile.to_csv('output.csv', sep=',')
+csvFile.to_csv('product_sold_' + startDate + '_to_' + endDate + '.csv', sep=',')
 wait = input("Wait for input")
